b9cc2c49e86a68b00d8deb7ae35397c3a02d4b58
longjzh <575598642@qq.com>
2024-05-22 21:04:43 +0800

    Test7-1

---
+++ Test7-1/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace YourNamespace
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (labelColorDisplay != null)
+            {
+                var color = Color.FromArgb(
+                    (byte)sliderAlpha.Value,
+                    (byte)sliderRed.Value,
+                    (byte)sliderGreen.Value,
+                    (byte)sliderBlue.Value);
+                labelColorDisplay.Background = new SolidColorBrush(color);
+            }
+        }
+    }
+}
